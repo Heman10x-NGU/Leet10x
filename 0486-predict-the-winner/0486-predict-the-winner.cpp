f47dc4bb6e5c320c8d43d9feeed81a97dbc33b9b
Heman10x-NGU <80228113+Heman10x-NGU@users.noreply.github.com>
2023-07-28 23:37:44 +0530

    Time: 3 ms (70.47%), Space: 7.5 MB (55.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<int>& nums){ // function is jjust for player1
+    int solve(int i,int j,vector<int>& nums,vector<vector<int>>& dp){ // function is jjust for player1
       if(i>j){
         return 0;
       }
@@ -8,18 +8,23 @@ public:
       if(i==j){
         return nums[i];
       }
-      int take_i = nums[i] + min(solve(i+2,j,nums) , solve(i+1,j-1,nums));
-      int take_j = nums[j] + min(solve(i,j-2,nums) , solve(i+1,j-1,nums));
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+      int take_i = nums[i] + min(solve(i+2,j,nums,dp) , solve(i+1,j-1,nums,dp));
+      int take_j = nums[j] + min(solve(i,j-2,nums,dp) , solve(i+1,j-1,nums,dp));
 
       //here we tried to do our best, and we expected the worst so took min
 
-      return max(take_i,take_j);
+      return dp[i][j]=max(take_i,take_j);
     }
     bool PredictTheWinner(vector<int>& nums) {
         int total=accumulate(nums.begin(),nums.end(),0);
         int n=nums.size();
-        int player1_Score=solve(0,n-1,nums);
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        int player1_Score=solve(0,n-1,nums,dp);
         int player2_Score=total-player1_Score;
+        
         
         return player1_Score>=player2_Score;
     }
